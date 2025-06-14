18540a6d18ecdbc72d70188f2f37d11c5a8b5abf
SubhamJoshi18 <shubhamrajjoshi69@gmail.com>
2025-06-14 15:42:04 +0545

    add the rabbitmq broker

---
+++ CV-Matcher-Backend/src/queues/serviceManager.ts
@@ -0,0 +1,55 @@
+import amqp from "amqplib";
+import { cvLogger } from "../libs/logger/logger.libs";
+import { getEnvValue } from "../utils/env.utils";
+
+class Broker {
+  public connection: amqp.ChannelModel | null = null;
+  public channel: amqp.Channel | null = null;
+
+  public async initalizeBroker() {
+    let retryCount = 5;
+    let attempts = 0;
+    while (attempts < retryCount) {
+      try {
+        attempts++;
+        this.connection = await amqp.connect(getEnvValue("AMQP_URL") as string);
+        this.channel = await this.connection.createChannel();
+      } catch (err: any) {
+        const isMaximumExceeded = attempts.toString().startsWith("0");
+        if (isMaximumExceeded) {
+          cvLogger.error(
+            `Maximum Retry Exceeded, Terminating the Server at retries ${retryCount}`
+          );
+        }
+        retryCount = retryCount - 1;
+        continue;
+      } finally {
+        cvLogger.info(`RabbitMQ Broker Initalized SuccessFully`);
+      }
+    }
+  }
+
+  public async getBrokerConnection() {
+    return new Promise(async (resolve, reject) => {
+      if (!this.connection) {
+        await this.initalizeBroker();
+      }
+      resolve(this.connection);
+    });
+  }
+
+  public async getBrokerChannel() {
+    return new Promise(async (resolve, reject) => {
+      if (!this.channel) {
+        await this.initalizeBroker();
+      }
+      resolve(this.channel);
+    });
+  }
+}
+
+const broker = () => {
+  return new Broker();
+};
+
+export default broker;
